77906a2d4dec4eb98a26d4f2e448ec59eb3bb2a0
Alexander Fok <foktmalexander@gmail.com>
2020-12-01 12:03:32 -0800

    Add layout and functions

---
+++ script.js
@@ -0,0 +1,38 @@
+let quizQuestions = {
+    "0": {
+        question:"",
+        choices:["","","",""],
+        answer:""
+    },
+    "1": {
+        question:"",
+        choices:["","","",""],
+        answer:""
+    },
+    "2": {
+        question:"",
+        choices:["","","",""],
+        answer:""
+    },
+    "3": {
+        question:"",
+        choices:["","","",""],
+        answer:""
+    }
+}
+
+let toTitle = ()=> {
+
+}
+
+let toNextQuestion = () => {
+    
+}
+
+let toScore = () => {
+
+}
+
+let toHighScore = () => {
+
+}
