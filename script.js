cf72240564d77384684826c0d9ad52e0983ec5b9
Alexander Fok <foktmalexander@gmail.com>
2020-12-01 12:26:34 -0800

    Add in questions for the quiz game

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 let quizQuestions = {
     "0": {
-        question:"",
-        choices:["","","",""],
-        answer:""
+        question:"Commonly used data types DO NOT include: ",
+        choices:["strings","booleans","alerts","numbers"],
+        answer:"alerts"
     },
     "1": {
-        question:"",
-        choices:["","","",""],
-        answer:""
+        question:"The condition in if/else statement is enclosed within ________.",
+        choices:["quotes","curly brackets","parentheses","square brackets"],
+        answer:"parentheses"
     },
     "2": {
-        question:"",
-        choices:["","","",""],
-        answer:""
+        question:"Arrarys in Javascript can be used to store ________.",
+        choices:["numbers and strings","other arrays","booleans","all of the above"],
+        answer:"all of the above"
     },
     "3": {
-        question:"",
-        choices:["","","",""],
-        answer:""
+        question:"String value can be enclosed within ________ when being assigned to variables",
+        choices:["commas","curly brackets","quotes","parentheses"],
+        answer:"quotes"
+    },
+    "4": {
+        question:"A very useful tool used during development and debugging for printing content to debugger is: ",
+        choices:["JavaScript","terminal / bash","for loop","console log"],
+        answer:"console log"
     }
 }
 
