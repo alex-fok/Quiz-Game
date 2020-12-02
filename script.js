e884f6a2a7f3d6fc00ed62973ade849fd1a24af5
Alexander Fok <foktmalexander@gmail.com>
2020-12-01 16:53:28 -0800

    Add logic for advancing from title -> question and question -> question. Add basic logic for timer

--- script.js
+++ script.js
@@ -23,15 +23,43 @@ let quizQuestions = {
         question:"A very useful tool used during development and debugging for printing content to debugger is: ",
         choices:["JavaScript","terminal / bash","for loop","console log"],
         answer:"console log"
-    }
+    },
+    "length" : 5
 }
 
-let toTitle = ()=> {
+const PAGE_CONTENT = ["info", "inGame"];
+const MAX_TIME = 60;
 
+let onQuestion = -1;
+let timer = -1;
+let timerID = 0;
+
+let showPageContent = (name) => {
+    PAGE_CONTENT.forEach((content)=> {
+        name===content ?
+        document.getElementById(content).style="display: block" :
+        document.getElementById(content).style="display: none";
+    })
+}
+
+let toTitle = () => {
+    showPageContent("info");
+    onQuestion = -1;
 }
 
 let toNextQuestion = () => {
-    
+    if (++onQuestion >= quizQuestions.length){
+        toHighScore();
+    }
+    else {
+        let qa = quizQuestions[onQuestion];
+        document.getElementById("inGameTitle").textContent = qa.question;
+        qa.choices.forEach( (choice, i) => {
+            let element =  document.getElementById("choice_"+ (i+1));
+            element.value = choice;
+            element.textContent = (i+1) + ". " + choice;
+        })
+    }
 }
 
 let toScore = () => {
@@ -39,5 +67,36 @@ let toScore = () => {
 }
 
 let toHighScore = () => {
-
+    console.log("to High Score");
 }
+
+let giveScore = () => {
+    
+}
+
+let resetTimer = () => {
+    timer = MAX_TIME;
+}
+
+(()=>{
+    document.getElementById("startQuiz").addEventListener('click',()=>{
+        showPageContent("inGame");
+        toNextQuestion();
+        resetTimer();
+        timerID = (setInterval(()=>{
+            timer -= 1;
+        }),1000);
+    })
+    let choices = document.getElementsByClassName("choice");
+    
+    Array.prototype.forEach.call(choices, (choice)=>{
+        choice.addEventListener('click', (event)=>{
+            
+            if (quizQuestions[onQuestion].answer !== event.target.value)
+                timer -= 10;
+            
+            console.log(timer);
+            toNextQuestion();
+        })
+    });
+})()
