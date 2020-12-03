5d3c7b903bb923fa9c431f13519f821b17ebd204
Alexander Fok <foktmalexander@gmail.com>
2020-12-02 22:47:10 -0800

    Static data moved to data.js

---
+++ data.js
@@ -0,0 +1,32 @@
+const PAGE_CONTENT = ["title", "inGame", "scoreReport", "highScores"];
+const MAX_TIME = 75;
+const PENALTY = 10;
+
+const quizQuestions = {
+    "0": {
+        question:"Commonly used data types DO NOT include: ",
+        choices:["strings","booleans","alerts","numbers"],
+        answer:"alerts"
+    },
+    "1": {
+        question:"The condition in if/else statement is enclosed within ________.",
+        choices:["quotes","curly brackets","parentheses","square brackets"],
+        answer:"parentheses"
+    },
+    "2": {
+        question:"Arrarys in Javascript can be used to store ________.",
+        choices:["numbers and strings","other arrays","booleans","all of the above"],
+        answer:"all of the above"
+    },
+    "3": {
+        question:"String value can be enclosed within ________ when being assigned to variables",
+        choices:["commas","curly brackets","quotes","parentheses"],
+        answer:"quotes"
+    },
+    "4": {
+        question:"A very useful tool used during development and debugging for printing content to debugger is: ",
+        choices:["JavaScript","terminal / bash","for loop","console log"],
+        answer:"console log"
+    },
+    "length" : 5
+}
